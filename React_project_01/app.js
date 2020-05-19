87205365f394f8490187fb050b2d4cbd80b1b6b3
Asget Dev <asgedomehaile@gmail.com>
2020-05-18 20:38:28 -0700

    Project_one

---
+++ React_project_01/app.js
@@ -0,0 +1,28 @@
+const title = React.createElement(
+    'h1',
+    {id:'main-title',title:'This is a title.'},
+    'My First React Element'
+);
+
+ReactDOM.render(
+    title,
+    document.getElementById('root')
+);
+
+const desc = React.createElement(
+    'p',
+    null,
+    'I just learned how to create a React node and render it into DOM.'
+);
+
+const header = React.createElement(
+    'header',
+    null,
+    title,
+    desc
+);
+
+ReactDOM.render(
+    header,
+    document.getElementById('root')
+);
